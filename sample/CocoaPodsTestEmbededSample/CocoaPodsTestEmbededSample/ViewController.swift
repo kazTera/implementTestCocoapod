137fd4dc1ac50d64b913109f7b707f3f6a80da72
kazunoriyamamoto <kazunori.yamamoto@geniee.co.jp>
2018-05-14 23:11:09 +0900

    add sample project

---
+++ sample/CocoaPodsTestEmbededSample/CocoaPodsTestEmbededSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPodsTestEmbededSample
+//
+//  Created by Yamamoto Kazunori on 2018/05/14.
+//  Copyright © 2018年 Yamamoto Kazunori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
