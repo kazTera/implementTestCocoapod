6e41882f886f1c256f07e83f313bac8cf00715b5
kazunoriyamamoto <kazunori.yamamoto@geniee.co.jp>
2018-05-14 21:31:52 +0900

    add framework

---
+++ GNAdSwipeInterstitialSDK.framework/Headers/GNAdSwipeInterstitialSDK.h
@@ -0,0 +1,19 @@
+//
+//  GNAdSwipeInterstitialSDK.h
+//  GNAdSwipeInterstitialSDK
+//
+//  Created by Yamamoto Kazunori on 2018/05/07.
+//  Copyright © 2018年 Yamamoto Kazunori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GNAdSwipeInterstitialSDK.
+FOUNDATION_EXPORT double GNAdSwipeInterstitialSDKVersionNumber;
+
+//! Project version string for GNAdSwipeInterstitialSDK.
+FOUNDATION_EXPORT const unsigned char GNAdSwipeInterstitialSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GNAdSwipeInterstitialSDK/PublicHeader.h>
+
+
